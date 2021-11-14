2640661d6418dd49c160c761180256770aa953bd
Anna Luchechko <annaluchechko@gmail.com>
2021-11-14 19:29:35 +0200

    Remove unused protocols and extension
    Removed unused protocols DefaultInitializable, Mockable and extension StaticFactory as it was copied by mistake
    Removed DefaultInitializable folder and renamed file DefaultInitializable to StaticFactory

---
+++ GitHubObserver/Utils/StaticFactory.swift
@@ -0,0 +1,12 @@
+//
+//  StaticFactory.swift
+//  GitHubObserver
+//
+//  Created by Anna Luchechko on 31.10.2021.
+//
+
+import Foundation
+
+protocol StaticFactory {
+    associatedtype Factory
+}
